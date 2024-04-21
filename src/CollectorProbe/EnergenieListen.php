b8c27656281ddf636a03dd000faab0edd7947281
Shane Mc Cormack <shanemcc@gmail.com>
2024-04-21 22:30:15 +0100

    Import Energenie-Listen code.

--- src/CollectorProbe/EnergenieListen.php
+++ src/CollectorProbe/EnergenieListen.php
@@ -3,9 +3,27 @@
 namespace CollectorProbe;
 
 class EnergenieListen extends AbstractProbe {
-	public function __construct() { }
+	private $discoveryFiles;
+
+	public function __construct($options = []) {
+		$this->discoveryFiles = $options['discoveryFiles'] ?? array('/tmp/energenie-listen.json');
+	}
 
 	public function getDevices() {
-		return [];
+		foreach ($this->discoveryFiles as $file) {
+			$json = json_decode(file_get_contents($file), true);
+
+			foreach ($json as $serial => $data) {
+				if ($serial == '__META') { continue; }
+
+				$dev = array();
+				$dev['name'] = $serial;
+				$dev['serial'] = $serial;
+				$dev['datasource'] = ['type' => 'energenie-listen'];
+				$dev['data'] = $data['data'];
+
+				yield $dev;
+			}
+		}
 	}
 }
