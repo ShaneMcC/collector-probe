b6b44e87d81e6d981ccfeb0a9f5df90198e4ac23
Shane Mc Cormack <shane@shanemcc.co.uk>
2026-05-01 19:59:20 +0100

    Fix config.

--- config.php
+++ config.php
@@ -62,7 +62,7 @@
 	// $probes[] = new \CollectorProbe\Tasmota('192.168.1.7', ['username' => 'someuser', 'password' => 'somepassword', 'zigbee' => true]);
 
 	/** Collect data from Shelly Plus Plug devices. */
-	$probes[] = new \CollectorProbe\Shelly('10.0.0.88');
+	// $probes[] = new \CollectorProbe\Shelly('192.168.1.8');
 
 	if (file_exists(dirname(__FILE__) . '/config.user.php')) {
 		require_once(dirname(__FILE__) . '/config.user.php');
